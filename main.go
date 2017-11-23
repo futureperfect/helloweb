0672fe859c252ba5c81399f6fa8abf2da7bee185
Erik Hollembeak <erik.hollembeak@gmail.com>
2017-11-22 18:51:36 -0800

    Add healthcheck route

--- main.go
+++ main.go
@@ -12,9 +12,14 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, world!\n")
 }
 
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "ok")
+}
+
 func main() {
 	hostPort := net.JoinHostPort("0.0.0.0", os.Getenv("PORT"))
 	http.HandleFunc("/", helloWorld)
+	http.HandleFunc("/healthy", healthCheck)
 
 	log.Println("Starting helloweb app")
 	log.Printf("Listening on %s\n", hostPort)
